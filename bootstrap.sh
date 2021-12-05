26884eba55835bbba5f70899492376ff1f26b706
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2021-12-05 13:45:02 +0900

    update: update script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,52 @@
 #!/usr/bin/env zsh
 
-echo -e "\e[1;3;36mWelcom!!\e[m"
-# echo -e "\e[1;3;36mWelcom!!\e[m"
-# echo -e "\e[1;3;36mStart of deployment.\e[m"
+set -e
 
-# List
-dotfiles=(.zshrc)
+DOTBASE=$HOME/dotfiles
+ENV=$(uname)
+
+echo "=============================================================================="
+echo "Start Installation."
+# Environmental determination Mac or WSL or Linux
+if [ $ENV == 'Darwin' ]; then
+  echo "Your environment is a Mac. Start deployment for macOS."
+  # Run install script
+  sh ./setup-scripts/install-mac.sh
+elif [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
+  echo "Your environment is a Windows Subsystem for Linux. Start deployment for WSL."
+  cd $HOME
+  # Update packages
+  sudo apt update -y && sudo apt upgrade -y
+  sudo apt install git -y
+  # Clone dotfile repository locally
+  git clone https://github.com/ryu-461/dotfiles.git
+  cd dotfiles
+  # Run install script
+  sh ./setup-scripts/install-wsl.sh
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+  cd $HOME
+  echo "Your environment is a Start deployment for Linux."
+  # Update packages
+  sudo apt update -y && sudo apt upgrade -y
+  sudo apt install git -y
+  # Clone dotfile repository locally
+  git clone https://github.com/ryu-461/dotfiles.git
+  # Run install script
+  sh ./setup-scripts/install-linux.sh
+else
+  exit 1
+fi
+echo "Installation complete."
+echo "=============================================================================="
+echo "Start Deployment."
 
 # Create simlink
 for file in "${dotfiles[@]}"; do
   [[ "$f" == ".git" ]] && continue
+  [[ "$f" == ".gitignore" ]] && continue
   [[ "$f" == ".DS_Store" ]] && continue
   ln -svf ~/dotfiles/${file} ~/${file}
 done
 
-echo -e "\e[1;3;36mDeployment complete.\e[m"
-echo -e "\e[1;3;36mHappy Hacking!!\e[m"
+echo "Deployment complete."
+echo "Happy Hacking!!"
