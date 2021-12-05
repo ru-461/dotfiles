63b42a824f242c192752f52695f59f4d7b22ce1b
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2021-12-05 13:42:32 +0900

    add: add new install scripts

---
+++ install-scripts/install-linux.sh
@@ -0,0 +1,19 @@
+# #!/usr/bin/env zsh
+# set -eux
+
+# install Homebrew
+which -s brew
+if [[ $? != 0 ]] ; then
+    echo "Linuxbrew is missing, install a new one."
+    sudo apt install build-essential curl file git
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "Setting up."
+    test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+    test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+    echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+    exec $SHELL -l
+else
+    echo "Skip to Linuxbrew."
+    brew update
+fi
---
+++ install-scripts/install-mac.sh
@@ -0,0 +1,22 @@
+# #!/usr/bin/env zsh
+# set -eux
+
+# install Homebrew
+which -s brew
+if [[ $? != 0 ]] ; then
+    echo "Homebrew is missing, install a new one."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "Upgrading formula..."
+    echo "brew upgrade"
+    brew upgrade
+    echo "brew cleanup"
+    brew cleanup
+    echo "brew doctor"
+    brew doctor
+    echo "done. "
+    echo "Install the package from Brewfile."
+    brew bundle --file '../Brewfile'
+else
+    echo "Skip to Homebrew."
+    brew update
+fi
