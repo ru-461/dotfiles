65e21edc39610de7c48a31fb9f0ac0c0998c1621
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2021-12-06 23:25:00 +0900

    update: adjust indent style & change script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,39 +1,38 @@
-#!/usr/bin/env zsh
+#!/bin/bash
 
-set -e
+set -ue
 
-DOTBASE=$HOME/dotfiles
-ENV=$(uname)
+DOT_BASE=$HOME/dotfiles
 
 echo "=============================================================================="
 echo "Start Installation."
 # Environmental determination Mac or WSL or Linux
-if [ $ENV == 'Darwin' ]; then
-  echo "Your environment is a Mac, Start deployment for macOS."
-  # Run install script
-  sh $DOTBASE/install-scripts/install-mac.sh
+if [ $(uname) == 'Darwin' ]; then
+	echo "Your environment is a Mac, Start deployment for macOS."
+	# Run install script
+	sh $DOT_BASE/install-scripts/install-mac.sh
 elif [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
-  echo "Your environment is a Windows Subsystem for Linux, Start deployment for WSL."
-  cd $HOME
-  # Update packages
-  sudo apt update -y && sudo apt upgrade -y
-  sudo apt install git -y
-  # Clone dotfile repository locally
-  git clone https://github.com/ryu-461/dotfiles.git
-  # Run install script
-  sh $DOTBASE/install-scripts/install-wsl.sh
+	echo "Your environment is a Windows Subsystem for Linux, Start deployment for WSL."
+	cd $HOME
+	# Update packages
+	sudo apt update -y && sudo apt upgrade -y
+	sudo apt install git -y
+	# Clone dotfile repository locally
+	git clone https://github.com/ryu-461/dotfiles.git
+	# Run install script
+	sh $DOT_BASE/install-scripts/install-wsl.sh
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-  cd $HOME
-  echo "Your environment is a Linux, Start deployment for Linux."
-  # Update packages
-  sudo apt update -y && sudo apt upgrade -y
-  sudo apt install git -y
-  # Clone dotfile repository locally
-  git clone https://github.com/ryu-461/dotfiles.git
-  # Run install script
-  sh $DOTBASE/setup-scripts/install-linux.sh
+	cd $HOME
+	echo "Your environment is a Linux, Start deployment for Linux."
+	# Update packages
+	sudo apt update -y && sudo apt upgrade -y
+	sudo apt install git -y
+	# Clone dotfile repository locally
+	git clone https://github.com/ryu-461/dotfiles.git
+	# Run install script
+	sh $DOT_BASE/setup-scripts/install-linux.sh
 else
-  exit 1
+	exit 1
 fi
 echo "Installation complete."
 echo "=============================================================================="
@@ -42,10 +41,11 @@ echo "Start Deployment."
 # Create simlink
 dotfiles=(.zshrc)
 for file in "${dotfiles[@]}"; do
-  [[ "$f" == ".git" ]] && continue
-  [[ "$f" == ".gitignore" ]] && continue
-  [[ "$f" == ".DS_Store" ]] && continue
-  ln -svf ~/dotfiles/${file} ~/${file}
+	[[ "$file" == ".git" ]] && continue
+	[[ "$file" == ".gitignore" ]] && continue
+	[[ "$file" == ".DS_Store" ]] && continue
+	echo "Deploying $file => $HOME"
+	ln -svf ~/dotfiles/${file} ~/${file}
 done
 
 echo "Deployment complete."
