69932cf3e07eeedb2cde36ad72c509e8c43a6187
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2021-12-07 00:43:18 +0900

    update: update judge archscript

--- install-scripts/install-mac.sh
+++ install-scripts/install-mac.sh
@@ -1,22 +1,40 @@
 #!/usr/bin/env bash
 
 set -ue
-# install Homebrew
-which -s brew
-if [[ $? != 0 ]] ; then
-  echo "Homebrew is missing, install a new one."
+
+UNAME=`uname -m`
+
+# Architecture determination
+if [[ $UNAME == arm64 ]]; then
+  # Install command line tools
+  if !(type "git" > /dev/null 2>&1); then
+    echo "Installing Command line tools."
+    xcode-select --install
+    # Install Rosetta2
+    echo "Installing Rosetta2."
+    /usr/sbin/softwareupdate --install-rosetta --agree-to-license
+  else
+    echo "Skip the command line tools as they are already installed."
+  fi
+else
+  echo "This script is not compatible with this architecture."
+  exit 1
+fi
+
+# Install Homebrew
+if !(type "brew" > /dev/null 2>&1); then
+  echo "Installing Homebrew."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo "Upgrading formula..."
-  echo "brew upgrade"
+  echo "brew upgrade."
   brew upgrade
-  echo "brew cleanup"
+  echo "brew cleanup."
   brew cleanup
-  echo "brew doctor"
+  echo "brew doctor."
   brew doctor
-  echo "done. "
-  echo "Install the package from Brewfile."
+  echo "done."
+  echo "Installing a package from Brewfile."
   brew bundle --file '../Brewfile'
 else
-  echo "Skip to Homebrew."
-  brew update
+  echo "Skip the Homebrew as they are already installed."
 fi
