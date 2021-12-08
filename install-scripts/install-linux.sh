eafebfa1e6d5afe7000b75aa11bf08c0afdc4368
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2021-12-09 00:32:53 +0900

    update: update installing messages

--- install-scripts/install-linux.sh
+++ install-scripts/install-linux.sh
@@ -4,8 +4,8 @@ set -ue
 
 # Install Linuxbrew
 if !(type "brew" > /dev/null 2>&1); then
-  echo "Installing Linuxbrew."
-  sudo apt install build-essential curl file git
+  echo "Installing Linuxbrew..."
+  sudo apt install build-essential curl file git -y
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo "done."
   test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
@@ -19,9 +19,9 @@ fi
 
 # Install Zsh
 if !(type "zsh" > /dev/null 2>&1); then
-  echo "Installing Zsh."
+  echo "Installing Zsh..."
   brew install zsh
-  echo "Setting default."
+  echo "Setting default..."
   sudo sh -c 'echo $(brew --prefix)/bin/zsh >> /etc/shells'
   chsh -s $(brew --prefix)/bin/zsh
   echo "Loading Settings from .zshrc"
