e4b1a7c7ba6a663427e271268bbdb37ef0bf1e70
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2021-12-09 21:55:20 +0900

    add: add new deploy file

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -ue
+
+echo "Expanding symbolic links ..."
+
+# to home
+for file in .??*; do
+  [[ "$file" == ".DS_Store" ]] && continue
+  [[ "$file" == ".git" ]] && continue
+  [[ "$file" == ".gitignore" ]] && continue
+  ln -svfv "$(pwd)/$file" "$HOME/$file"
+done
+
+# to .Config
+mkdir -p $HOME/.config
+for file in "$(ls .config)"; do
+  ln -snfv "$(pwd)/.config/$file" "$HOME/.config/$file"
+done
+
+echo "Symbolic link expansion is complete."
