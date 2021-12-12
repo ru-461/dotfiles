10d03b4643a0e15c050bc9a2042d11dc9193d7f6
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2021-12-12 10:31:32 +0900

    update: Add git actions

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,12 +5,23 @@ set -ue
 DOT_BASE=$HOME/dotfiles
 DOT_REMOTE=https://github.com/ryu-461/dotfiles.git
 
-echo "=============================================================================="
+read -p "Welcome dotfiles installation!! This script will install and deploy the various packages. Are you ready? [y/N] ')" -n 1 -r
+if [ ! $REPLY =~ ^[Yy]$ ]; then
+  echo ""
+  echo 'The installation has been canceled. There is nothing to do.'
+  exit 1
+fi
+echo ""
 echo "Start Installation."
-
 # Environmental determination Mac or WSL or Linux
 if [ $(uname) == 'Darwin' ]; then
   echo "Your environment is a Mac, Start deployment for macOS."
+    # Clone dotfile repository locally
+  if [ ! -d $HOME/dotfiles ]; then
+    echo "Cloning the dotfiles repository ..."
+    cd $HOME
+    git clone $DOT_REMOTE
+  fi
   # Run install script
   # source $DOT_BASE/install-scripts/install-mac.sh
 elif [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
@@ -23,16 +34,12 @@ elif [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
   # Clone dotfile repository locally
   if [ ! -d $HOME/dotfiles ]; then
     echo "Cloning the dotfiles repository ..."
+    cd $HOME
     git clone $DOT_REMOTE
-  else
-    echo "Updating the dotfiles ..."
-    cd $DOT_BASE
-    git pull origin main
   fi
   # Run install script
   source $DOT_BASE/install-scripts/install-wsl.sh
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-  cd $HOME
   echo "Your environment is a Linux, Start deployment for Linux."
   # Update packages
   echo "Updating the package to the latest ..."
@@ -41,11 +48,17 @@ elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
   # Clone dotfile repository locally
   if [ ! -d $HOME/dotfiles ]; then
     echo "Cloning the dotfiles repository ..."
+    cd $HOME
     git clone $DOT_REMOTE
   else
-    echo "Updating the dotfiles ..."
-    cd $DOT_BASE
-    git pull origin main
+    read -p "The dotfiles already exists. Do you want to update them? [y/N] ')" -n 1 -r
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      echo "Updating the dotfiles ..."
+      cd $DOT_BASE
+      git pull origin main
+    fi
+    echo 'There is nothing to do.'
   fi
   # Run install script
   source $DOT_BASE/install-scripts/install-linux.sh
@@ -53,9 +66,7 @@ else
   exit 1
 fi
 echo "Installation complete."
-echo "=============================================================================="
 # Start deploy.
 cd $DOT_BASE
-source $DOT_BASE/deploy.sh
-
+# source $DOT_BASE/deploy.sh
 echo "Happy Hacking!!"
