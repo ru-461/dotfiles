e97c1337676cc8f5758969c78797be7e22b5f7de
ryu-461 <48552543+ryu-461@users.noreply.github.com>
2021-12-12 10:38:50 +0900

    update: Adjust branching

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,25 +6,24 @@ DOT_BASE=$HOME/dotfiles
 DOT_REMOTE=https://github.com/ryu-461/dotfiles.git
 
 read -p "Welcome dotfiles installation!! This script will install and deploy the various packages. Are you ready? [y/N] ')" -n 1 -r
-if [ ! $REPLY =~ ^[Yy]$ ]; then
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
   echo ""
   echo 'The installation has been canceled. There is nothing to do.'
   exit 1
 fi
 echo ""
 echo "Start Installation."
-# Environmental determination Mac or WSL or Linux
-if [ $(uname) == 'Darwin' ]; then
+if [[ $(uname) == 'Darwin' ]]; then
   echo "Your environment is a Mac, Start deployment for macOS."
     # Clone dotfile repository locally
-  if [ ! -d $HOME/dotfiles ]; then
+  if [[ ! -d $HOME/dotfiles ]]; then
     echo "Cloning the dotfiles repository ..."
     cd $HOME
     git clone $DOT_REMOTE
   fi
   # Run install script
   # source $DOT_BASE/install-scripts/install-mac.sh
-elif [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
+elif [[ -f /proc/sys/fs/binfmt_misc/WSLInterop ]]; then
   echo "Your environment is a Windows Subsystem for Linux, Start deployment for WSL."
   cd $HOME
   # Update packages
@@ -39,14 +38,14 @@ elif [ -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
   fi
   # Run install script
   source $DOT_BASE/install-scripts/install-wsl.sh
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+elif [[ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]]; then
   echo "Your environment is a Linux, Start deployment for Linux."
   # Update packages
   echo "Updating the package to the latest ..."
   sudo apt update -y && sudo apt upgrade -y
   sudo apt install git -y
   # Clone dotfile repository locally
-  if [ ! -d $HOME/dotfiles ]; then
+  if [[ ! -d $HOME/dotfiles ]]; then
     echo "Cloning the dotfiles repository ..."
     cd $HOME
     git clone $DOT_REMOTE
